4216d8bcff2cf048a950285ddcf8bdf6755a156a
Oscar <carrenooscar.95@gmail.com>
2023-03-08 20:35:44 -0500

    Actualizando el proyecto

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
 from . import views
-
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index")
+    #ex /polls/
+    path("", views.IndexView.as_view(), name="index"),
+    #ex /polls/#/detail
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    #ex /polls/#/results
+    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
+    #ex /polls/#/votes
+    path("<int:question_id>/votes/", views.votes, name="votes")
 ]
